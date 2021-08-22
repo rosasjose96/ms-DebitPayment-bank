991ff05e98ce01835b8d67f1dbfd255abb6bbfe8
rosasjose96 <34173505+rosasjose96@users.noreply.github.com>
2021-08-22 00:59:39 -0500

    initial commit

---
+++ src/main/java/com/bootcamp/msdebitpayment/MsDebitPaymentBankApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.msdebitpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDebitPaymentBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDebitPaymentBankApplication.class, args);
+	}
+
+}
